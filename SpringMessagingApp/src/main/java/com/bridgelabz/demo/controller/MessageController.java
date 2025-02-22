0e8390a78e2e1cff7620760022c6d19d47f93efe
R-scan1 <deepr7590@gmail.com>
2025-02-22 15:34:57 +0530

    [Raj] add. using get request method and passing name as query parameter

--- SpringMessagingApp/src/main/java/com/bridgelabz/demo/controller/MessageController.java
+++ SpringMessagingApp/src/main/java/com/bridgelabz/demo/controller/MessageController.java
@@ -2,6 +2,7 @@ package com.bridgelabz.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,10 @@ public class MessageController {
     @GetMapping
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+    
+    @GetMapping("/query")
+    public String sayHello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
